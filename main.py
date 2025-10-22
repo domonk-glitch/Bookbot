612aa878e5b97a1316617de2df9dd95ec61bad10
domonk-glitch <domonk@gmail.com>
2025-10-22 11:12:21 -0400

    word count works

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 file_contents = None
 
-def get_book_text(path):
+def get_word_count(path):
     with open(path) as f:
         file_contents = f.read()
-        print(file_contents)
+        words = file_contents.split()
+        count = len(words)
+        print(f"Found {count} total words")
+
+#def get_book_text(path):
+#    with open(path) as f:
+#        file_contents = f.read()
+#        print(file_contents)
 
 def main():
-    get_book_text("books/frankenstein.txt")
-
+#    get_book_text("books/frankenstein.txt")
+    get_word_count("books/frankenstein.txt")
 
 
 
