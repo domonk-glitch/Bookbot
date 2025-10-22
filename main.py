ba235aebc9fa631f4d69323e669a2b396d55d500
domonk-glitch <domonk@gmail.com>
2025-10-22 10:43:19 -0400

    got bot to read book

---
+++ main.py
@@ -0,0 +1,14 @@
+file_contents = None
+
+def get_book_text(path):
+    with open(path) as f:
+        file_contents = f.read()
+        print(file_contents)
+
+def main():
+    get_book_text("books/frankenstein.txt")
+
+
+
+
+main()
